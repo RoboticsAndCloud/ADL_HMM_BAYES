132bf201ed1fe9132ede943f260c02824ef81701
Your Name <you@example.com>
2022-11-10 16:40:36 -0600

    update post_training_float16_quant

--- TinyML/room_classifier/room_tflite_converter.py
+++ TinyML/room_classifier/room_tflite_converter.py
@@ -13,14 +13,17 @@ def representative_data_gen():
 
 # Convert the model
 saved_model_dir = './home-model'
-converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir) # path to the SavedModel directory
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-res_model = 'home_default_model.tflite'
+
+#converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir) # path to the SavedModel directory
+#converter.optimizations = [tf.lite.Optimize.DEFAULT]
+#res_model = 'home_default_model.tflite'
 
 # try
-#converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir) # path to the SavedModel directory
-#converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
-#res_model = 'home_for_size_model.tflite'
+# Refer: https://www.tensorflow.org/lite/performance/post_training_float16_quant
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir) # path to the SavedModel directory
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.target_spec.supported_types = [tf.float16]
+res_model = 'home_16_size_model.tflite'
 
 
 
