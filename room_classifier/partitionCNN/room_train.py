c5bfd8d4b7066c773195f65d0189c19ca89b9dd3
Your Name <you@example.com>
2023-12-20 14:33:47 -0600

    partition

--- room_classifier/partitionCNN/room_train.py
+++ room_classifier/partitionCNN/room_train.py
@@ -47,7 +47,7 @@ MAX_EPOCH = 20
 img_size = 299     # resize all the images to one size
 #img_size = 32     # resize all the images to one size
 
-def AlexnetModel1(input_shape=(IMG_WIDTH, IMG_HEIGHT, 3),num_classes=6, l2_reg=0.):
+def AModel1(input_shape=(IMG_WIDTH, IMG_HEIGHT, 3),num_classes=6, l2_reg=0.):
     # Initialize model
     alexnet = Sequential()
 
@@ -113,6 +113,8 @@ def AlexnetModel1(input_shape=(IMG_WIDTH, IMG_HEIGHT, 3),num_classes=6, l2_reg=0
 def AlexnetModel2(input_shape=(IMG_WIDTH, IMG_HEIGHT, 3),num_classes=6, l2_reg=0.):
     """
     https://github.com/visionatseecs/keras-starter/blob/main/keras_alexnet.ipynb
+    https://colab.research.google.com/gist/SuryanarayanaY/07c54cc27046514599476fc0edd9b58f/43379-sr1.ipynb#scrollTo=h4jjjeqNfGMc
+    https://github.com/pravinkr/alexnet-cifar10-using-keras/blob/master/cifar_10_with_Alexnet.ipynb
     """
     print('AlexnetModel2:')
     inputs = Input(shape=input_shape)
@@ -145,7 +147,8 @@ def AlexnetModel2(input_shape=(IMG_WIDTH, IMG_HEIGHT, 3),num_classes=6, l2_reg=0
     dense_3 = Dense(num_classes,activation='softmax', name='Part_Final')(dense_2)
 
     p_outputs = [conv_3, dense_3]
-    alexnet =  Model(inputs=inputs, outputs=dense_3)
+    #alexnet = Model(inputs=inputs, outputs=dense_3)
+    alexnet = Model(inputs=inputs, outputs=p_outputs)
     alexnet.summary()
 
     learning_rate = 0.001
@@ -187,13 +190,24 @@ def train():
     model = AlexnetModel2(input_shape = X.shape[1:])
     for layer in model.layers:
         print('Layer:', layer)
-    model.fit(X, class_num, epochs=15, batch_size=32,validation_split=0.2)
+    model.fit(X, class_num, epochs=25, batch_size=32,validation_split=0.2)
     print('model fit complete')
 
     MODEL_SAVED_PATH = 'watch-saved-model-alex'
     model.save(MODEL_SAVED_PATH)
     print("train AlexNet")
 
+    print('executing prediction')
+    test_img = './Images_test' + '/' + 'hunter_room.jpg'
+    #test_img = './Images_test' + '/' + 'bedroom.jpg'
+    test_img = './watch_data/Images_test' + '/' + 'kitchen.jpg'
+    #img_array = cv2.imread(test_img,cv2.IMREAD_GRAYSCALE)
+    img_array = cv2.imread(test_img,cv2.IMREAD_COLOR)
+    new_array = cv2.resize(img_array, (img_size, img_size))
+    new_array=new_array.reshape(-1,img_size, img_size,channel)
+    prediction = model.predict([new_array])
+    print(prediction)
+    print(categories[np.argmax(prediction)])
 
 train()
 exit(0)
@@ -232,34 +246,7 @@ dense_layers=[4]
 layer_sizes=[128]
 conv_layers=[2]
 
-for dense_layer in dense_layers:
-    for layer_size in layer_sizes:
-        for conv_layer in conv_layers:
-            print('----------------------------one')
-            name = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer,layer_size,dense_layer,int(time.time()))
-            print(name)
-            print('shape:', X.shape[1:])
-            tensorboard = TensorBoard(log_dir='.\logs{}'.format(name))
-            model = models.Sequential()
-            model.add(Conv2D(layer_size,(3,3), input_shape = X.shape[1:]))
-            model.add(Activation("relu"))
-            model.add(MaxPooling2D(pool_size=(2,2)))
-            for l in range(conv_layer-1):
-                model.add(Conv2D(layer_size,(3,3)))
-                model.add(Activation("relu"))
-                model.add(MaxPooling2D(pool_size=(2,2)))
-            model.add(Flatten())
-            for l in range(dense_layer):
-                model.add(Dense(layer_size, activation='relu'))
-            model.add(Dropout(0.5))
-            model.add(Dense(len(categories), activation='softmax'))   
-            model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
-            model.summary()
-            model.fit(X, class_num, epochs=15, batch_size=32,validation_split=0.2,callbacks=[tensorboard])
-            print('model fit complete')
 
-            MODEL_SAVED_PATH = 'watch-saved-model'
-            model.save(MODEL_SAVED_PATH)
 
 #predicting
 #Pass the test image or image for prediction below 
